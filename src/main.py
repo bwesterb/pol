571787856de6a84ae158f6e8e00aceb6f62c18a4
Bas Westerbaan <bas@westerbaan.name>
2013-04-02 00:13:27 +0200

    main: implement list, generate and paste.  some work on init

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 
     Contains the argument parser and CLI interaction. """
 
+import threading
 import argparse
 import logging
 import os.path
@@ -12,6 +13,9 @@ import pprint
 import sys
 
 import pol.safe
+import pol.passgen
+import pol.terminal
+import pol.clipboard
 import pol.progressbar
 
 class Program(object):
@@ -45,6 +49,17 @@ class Program(object):
         p_list = subparsers.add_parser('list',
                     help='List entries')
         p_list.set_defaults(func=self.cmd_list)
+
+        p_generate = subparsers.add_parser('generate',
+                    help='Generates and stores a password')
+        p_generate.add_argument('key')
+        p_generate.add_argument('--note', '-n')
+        p_generate.set_defaults(func=self.cmd_generate)
+
+        p_copy = subparsers.add_parser('copy',
+                    help='Copies a password to the clipboard')
+        p_copy.add_argument('key')
+        p_copy.set_defaults(func=self.cmd_copy)
 
         p_touch = subparsers.add_parser('touch',
                     help='Rerandomizes blocks')
@@ -101,22 +116,15 @@ class Program(object):
                                       precomputed_gp=self.args.precomputed_gp,
                                       use_threads=self.args.threads)
         # TODO stub
-        safe._new_slice(safe.nblocks).trash()
+        c = safe.new_container('p', 'l', 'a')
+        safe.trash_freespace()
         with open(os.path.expanduser(self.args.safe), 'w') as f:
             safe.store(f)
     
     def cmd_touch(self):
         with open(os.path.expanduser(self.args.safe)) as f:
             safe = pol.safe.Safe.load(f)
-        progressbar = pol.progressbar.ProgressBar()
-        progressbar.start()
-        def progress(v):
-            progressbar(v)
-            if v == 1.0:
-                progressbar.end()
-        safe.rerandomize(nworkers=self.args.workers,
-                         use_threads=self.args.threads,
-                         progress=progress)
+        self._rerandomize(safe)
         with open(os.path.expanduser(self.args.safe), 'w') as f:
             safe.store(f)
 
@@ -128,10 +136,118 @@ class Program(object):
             del d['blocks']
         pprint.pprint(d)
 
+    def cmd_copy(self):
+        with open(os.path.expanduser(self.args.safe)) as f:
+            safe = pol.safe.Safe.load(f)
+        found_one = False
+        entries = []
+        for container in safe.open_containers(
+                getpass.getpass('Enter password: ')):
+            if not found_one:
+                found_one = True
+            try:
+                for entry in container.get(self.args.key):
+                    entries.append((container, entry))
+            except pol.safe.MissingKey:
+                continue
+            except KeyError:
+                continue
+        if not found_one:
+            print 'The password did not open any container.'
+            return -1
+        if len(entries) == 1:
+            entry = entries[0][1]
+            print ' note: %s' % repr(entry[1])
+            print 'Copied secret to clipboard.  Press any key to clear ...'
+            pol.clipboard.copy(entry[2])
+            pol.terminal.wait_for_keypress()
+            pol.clipboard.clear()
+            return
+        print '%s entries found.' % len(entries)
+        print
+        first = True
+        for i, tmp in enumerate(entries):
+            if first:
+                first = False
+            else:
+                print
+            container, entry = tmp
+            print 'Entry #%s from container @%s' % (i+1, container.id)
+            print ' note: %s' % repr(entry[1])
+            print 'Copied secret to clipboard.  Press any key to clear ...'
+            pol.clipboard.copy(entry[2])
+            pol.terminal.wait_for_keypress()
+            pol.clipboard.clear()
+    def cmd_generate(self):
+        with open(os.path.expanduser(self.args.safe)) as f:
+            safe = pol.safe.Safe.load(f)
+        pw = pol.passgen.generate_password()
+        found_one = False
+        stored = False
+        for container in safe.open_containers(
+                getpass.getpass('Enter (append-)password: ')):
+            if not found_one:
+                found_one = True
+            try:
+                container.add(self.args.key, self.args.note, pw)
+                container.save()
+                stored = True
+            except pol.safe.MissingKey:
+                pass
+        if not found_one:
+            print 'The password did not open any container.'
+            return -1
+        if found_one and not stored:
+            print 'No append access to the containers opened by this password'
+            return -2
+        pol.clipboard.copy(pw)
+
+        finished = threading.Event()
+        def finish_up():
+            self._rerandomize(safe)
+            with open(os.path.expanduser(self.args.safe), 'w') as f:
+                safe.store(f)
+            finished.set()
+        threading.Thread(target=finish_up).start()
+        print 'Copied password to clipboard.  Press any key to clear ...'
+        pol.terminal.wait_for_keypress()
+        pol.clipboard.clear()
+        finished.wait()
+
     def cmd_list(self):
         with open(os.path.expanduser(self.args.safe)) as f:
             safe = pol.safe.Safe.load(f)
-        safe.open(getpass.getpass('Enter (list-)password: '))
+        found_one = False
+        for container in safe.open_containers(
+                getpass.getpass('Enter (list-)password: ')):
+            if not found_one:
+                found_one = True
+            else:
+                print
+            print 'Container @%s' % container.id
+            try:
+                got_entry = False
+                for key, note in container.list():
+                    got_entry = True
+                    print ' %-20s %s' % (key, note if note else '')
+                if not got_entry:
+                    print '  (empty)'
+            except pol.safe.MissingKey:
+                print '  (no list access)'
+        if not found_one:
+            print ' No containers found'
+
+    def _rerandomize(self, safe):
+        progressbar = pol.progressbar.ProgressBar()
+        progressbar.start()
+        def progress(v):
+            progressbar(v)
+            if v == 1.0:
+                progressbar.end()
+        safe.rerandomize(nworkers=self.args.workers,
+                         use_threads=self.args.threads,
+                         progress=progress)
+
 
 def entrypoint(argv):
     return Program().main(argv)

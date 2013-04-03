d0a04a9e27b55063ae063ea2673de9cc91a3ca3c
Bas Westerbaan <bas@westerbaan.name>
2013-04-03 22:58:36 +0200

    test_main: enable again

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -3,19 +3,22 @@ import tempfile
 
 import pol.main
 
-#class TestMain(unittest.TestCase):
-#    def setUp(self):
-#        self.safe = tempfile.NamedTemporaryFile()
-#        self.assertEqual(self.pol('init', '-P'), 0)
-#
-#    def pol(self, *args):
-#        ret = pol.main.entrypoint(['-s', self.safe.name] + list(args))
-#        if ret is None:
-#            return 0
-#    def test_touch(self):
-#        self.assertEqual(self.pol('touch'), 0)
-#    def test_raw(self):
-#        self.assertEqual(self.pol('raw'), 0)
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.safe = tempfile.NamedTemporaryFile()
+        self.pol('init', '-P', '-p', 'a', 'b', 'c', '-f')
+
+    def pol(self, *args):
+        ret = pol.main.entrypoint(['-s', self.safe.name] + list(args))
+        if ret is None:
+            return 0
+    def test_touch(self):
+        self.assertEqual(self.pol('touch'), 0)
+    def test_raw(self):
+        self.assertEqual(self.pol('raw'), 0)
+    def test_list(self):
+        self.assertEqual(self.pol('list', '-p', 'a'), 0)
+        self.assertEqual(self.pol('list', '-p', 'b'), 0)
 
 if __name__ == '__main__':
     unittest.main()

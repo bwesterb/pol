68899f02162e016fb6d9834a3a43c9e3d3942b9b
Bas Westerbaan <bas@westerbaan.name>
2013-04-21 16:03:21 +0200

    clipboard: add GTK support

--- src/clipboard.py
+++ src/clipboard.py
@@ -6,8 +6,8 @@ import subprocess
 
 l = logging.getLogger(__name__)
 
+# First, if we're on Mac, use pbcopy and pbpaste
 if sys.platform == 'darwin':
-    # Mac OS X
     available = True
     def copy(s):
         p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE)
@@ -19,38 +19,55 @@ if sys.platform == 'darwin':
     def paste():
         return subprocess.check_output(['pbpaste'])
 else:
+    # Then, try GTK
     try:
-        import Tkinter
-        got_tkinter = True
-        tk = Tkinter.Tk()
-        tk.withdraw()
-        tk.destroy()
+        import gtk
     except:
-        got_tkinter = False
-    if got_tkinter:
-        available = True
-        def clear():
-            tk = Tkinter.Tk()
-            tk.withdraw()
-            tk.clipboard_clear()
-            tk.destroy()
+        got_gtk = False
+
+    if got_gtk:
         def copy(s):
-            tk = Tkinter.Tk()
-            tk.withdraw()
-            tk.clipboard_clear()
-            tk.clipboard_append(s)
-            tk.destroy()
+            cb = gtk.Clipboard()
+            cb.set_text(s)
+            cb.store()
         def paste():
-            tk = Tkinter.Tk()
-            tk.withdraw()
-            ret = tk.clipboard_get()
-            tk.destroy()
-            return ret
+            return gtk.Clipboard().wait_for_text()
+        def clear():
+            copy('')
     else:
-        available = False
-        def copy(s):
-            raise NotImplementedError
-        def paste():
-            raise NotImplementedError
-        def clear():
-            raise NotImplementedError
+        # Finally, try Tkinter
+        try:
+            import Tkinter
+            got_tkinter = True
+            tk = Tkinter.Tk()
+            tk.withdraw()
+            tk.destroy()
+        except:
+            got_tkinter = False
+        if got_tkinter:
+            available = True
+            def clear():
+                tk = Tkinter.Tk()
+                tk.withdraw()
+                tk.clipboard_clear()
+                tk.destroy()
+            def copy(s):
+                tk = Tkinter.Tk()
+                tk.withdraw()
+                tk.clipboard_clear()
+                tk.clipboard_append(s)
+                tk.destroy()
+            def paste():
+                tk = Tkinter.Tk()
+                tk.withdraw()
+                ret = tk.clipboard_get()
+                tk.destroy()
+                return ret
+        else:
+            available = False
+            def copy(s):
+                raise NotImplementedError
+            def paste():
+                raise NotImplementedError
+            def clear():
+                raise NotImplementedError

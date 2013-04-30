a089e1f2498aa08717617c398147e149c50b51ec
Bas Westerbaan <bas@westerbaan.name>
2013-04-30 18:57:33 +0200

    test_main: improve a little bit

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -6,20 +6,29 @@ import pol.main
 class TestMain(unittest.TestCase):
     def setUp(self):
         self.safe = tempfile.NamedTemporaryFile()
-        self.pol('init', '-P', '-p', 'a', 'b', 'c', '-f',
-                    '--i-know-its-unsafe')
 
     def pol(self, *args):
         ret = pol.main.entrypoint(['-s', self.safe.name] + list(args))
-        if ret is None:
-            return 0
-    def test_touch(self):
-        self.assertEqual(self.pol('touch'), 0)
-    def test_raw(self):
-        self.assertEqual(self.pol('raw'), 0)
-    def test_list(self):
+        return 0 if ret is None else ret
+    def test_basic(self):
+        # TODO we should check output
+        self.pol('init', '-P', '-p', 'a', 'b', 'c', '-f',
+                    '--i-know-its-unsafe', '-N', '128')
         self.assertEqual(self.pol('list', '-p', 'a'), 0)
         self.assertEqual(self.pol('list', '-p', 'b'), 0)
+        self.assertEqual(self.pol('put', '-p', 'a', '-s', 'a secret', 'key'), 0)
+        self.assertEqual(self.pol('get', '-p', 'a', 'key'), 0)
+        self.assertEqual(self.pol('put', '-p', 'b', '-s', 'a secret', 'key'), 0)
+        self.assertEqual(self.pol('put', '-p', 'c', '-s', 'a secret', 'key'), 0)
+        self.assertEqual(self.pol('put', '-p', 'd', '-s', 'a secret', 'key'), -1)
+        self.assertEqual(self.pol('get', '-p', 'a', 'key'), -8)
+        self.assertEqual(self.pol('get', '-p', 'b', 'key'), -4)
+        self.assertEqual(self.pol('get', '-p', 'c', 'key'), -4)
+        self.assertEqual(self.pol('list', '-p', 'a'), 0)
+        self.assertEqual(self.pol('list', '-p', 'b'), 0)
+        self.assertEqual(self.pol('touch'), 0)
+        self.assertEqual(self.pol('export', '-p', 'a'), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
